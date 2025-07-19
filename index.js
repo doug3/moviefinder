f4e928076270967974d80844118751cc24fa0135
doug3 <doug.corcoran@hotmail.com>
2025-07-19 14:01:26 -0400

    movieFinder v2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const movieListElement = document.querySelector(".movie__list");
+const searchBar = document.querySelector("#searchBar");
 
 function openMenu() {
     document.body.classList.add('menu--open');
@@ -11,30 +12,34 @@ function closeMenu() {
 
 async function onSearch() {
     
-    const searchTitle = document.getElementById('searchBar').value;
+    const searchTitle = searchBar.value;
     const searchYear = document.getElementById('searchYear').value;
     var searchKey = "s=" + searchTitle + "&type=movie";
     if (searchYear) searchKey += ("&y=" + searchYear);
+    if (searchTitle) {
+        try {
+            const movies = await fetch("https://www.omdbapi.com/?apikey=8097d20a&" + searchKey);
+            const moviesData = await movies.json();
 
-    try {
-        const movies = await fetch("https://www.omdbapi.com/?apikey=8097d20a&" + searchKey);
-        const moviesData = await movies.json();
+            if (moviesData.Search.length > 6) moviesData.Search.length = 6;
+            
+            
+            // if ((Array.isArray(moviesData.Search)) && (moviesData.Search.length > 1)) {
+            if (Array.isArray(moviesData.Search)) {
+                movieListElement.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
 
-        if (moviesData.Search.length > 6) moviesData.Search.length = 6;
-        
-        
-        if ((Array.isArray(moviesData.Search)) && (moviesData.Search.length > 1)) {
-            movieListElement.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
-        } else if (moviesData.Search.length == 1) {
-            movieListElement.innerHTML = movieHTMLSingle(moviesData);
-        } else {
-            movieListElement.innerHTML = "<p>No results found.</p>";
+            // } else if (moviesData.Search.length == 1) {
+            //     movieListElement.innerHTML = movieHTMLsingle(moviesData);
+            
+            } else {
+                movieListElement.innerHTML = "<p>No results found.</p>";
+            }
         }
-    }
-    catch (error) {
-        console.log("There was a problem!! Try again.");
-    }
-}   
+        catch (error) {
+            console.log("There was a problem!! Try again.");
+        }
+    } else alert("Title is required to search");   
+}
 
 function movieHTML(movie) {
     return `
@@ -47,18 +52,19 @@ function movieHTML(movie) {
 }
 
 
-function movieHTMLSingle(movie) { 
-    return `
-            <div class="movie__box">
-                <img src="${movie.Poster}" class="movie__poster"></img>
-                <h2 class="movie__title">${movie.Title}</h2>
-                <p>Year: ${movie.Year}</p>
-                <p>Rating: ${movie.Rated || "N/A"}</p>
-                <p>Length: ${movie.Runtime || "N/A"}</p>
-                <p class="movie__plot">Plot: ${movie.Plot || "N/A"}</p>
-            </div>
-            `;
-}
+// function movieHTMLSingle(movie) { 
+//     console.log(movie);
+//     return `
+//             <div class="movie__box">
+//                 <img src="${movie.Poster}" class="movie__poster"></img>
+//                 <h2 class="movie__title">${movie.Title}</h2>
+//                 <p>Year: ${movie.Year}</p>
+//                 <p>Rating: ${movie.Rated || "N/A"}</p>
+//                 <p>Length: ${movie.Runtime || "N/A"}</p>
+//                 <p class="movie__plot">Plot: ${movie.Plot || "N/A"}</p>
+//             </div>
+//             `;
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
     movieListElement.addEventListener('error', function (e) {
@@ -66,4 +72,12 @@ document.addEventListener('DOMContentLoaded', () => {
             e.target.src = './assets/unavailable.png';
         }
     }, true);
+});
+
+searchBar.addEventListener("keyup", function(event) {
+    console.log(event.code);
+    if ((event.code === "Enter") || (event.code === "NumpadEnter")) {
+        event.preventDefault();
+        onSearch();
+    }
 });
